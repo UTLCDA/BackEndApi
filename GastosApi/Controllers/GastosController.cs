c1474b5ec964fa10163c0579ade589e96e47b16a
Aaron Arenas Martinez <aaronarenasmartinez@gmail.com>
2024-03-27 22:38:48 -0600

    Agregar archivos de proyecto.

---
+++ GastosApi/Controllers/GastosController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GastosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GastosController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Get(decimal gasto, decimal gastoDos) {
+        
+        return gasto + gastoDos;    
+
+        }
+
+        [HttpPost]
+        public decimal Add(decimal gasto, decimal gastoDos)
+        {
+
+            return gasto + gastoDos;
+
+        }
+
+        [HttpPut]
+        public decimal Edit(decimal gasto, decimal gastoDos)
+        {
+
+            return gasto + gastoDos;
+
+        }
+
+        [HttpDelete]
+        public decimal Delete(decimal gasto, decimal gastoDos)
+        {
+
+            return gasto + gastoDos;
+
+        }
+    }
+}
